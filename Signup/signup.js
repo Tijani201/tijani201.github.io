493dc606ae6adcd5e502667f20b10d209f148ab4
Tijani <tijaniusat4barcelona@gmail.com>
2022-04-26 08:30:48 +0100

    Fetch School Api

---
+++ Signup/signup.js
@@ -0,0 +1,60 @@
+const label = document.querySelectorAll(".container");
+const firstName = document.getElementById("firstName").value;
+const lastName = document.getElementById("lastName").value;
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const confirmPassword = document.getElementById("confirmPassword").value;
+
+const button = document.getElementById("submit");
+const form = document.getElementById("form");
+
+let apiUrl = "https://sch-finder-api.herokuapp.com";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+    confirmPassword: document.getElementById("confirmPassword").value,
+  };
+
+  signUp(data);
+});
+
+function signUp(data) {
+  fetch(`${apiUrl}/api/users/signup`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json().then((json) => {
+          saveToken(json.token);
+          reDirect();
+        });
+      }
+      throw new Error("Something went wrong");
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function saveToken(token) {
+  console.log(token);
+  window.localStorage.setItem("token", token);
+}
+
+function getToken() {
+  return window.localStorage.getItem("token");
+}
+
+function reDirect() {
+  window.location.href = "../SignIn/signin.html";
+}
